69cf7ee6b86de2a007b7863664b66678ad06774e
pjmurphy777 <pjmurphy77@gmail.com>
2020-02-04 09:58:37 -0500

    Update index.php

--- grounded/index.php
+++ grounded/index.php
@@ -2,7 +2,7 @@
 <?php
 $title = 'Grounded Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'gmbgmgpeeflmnokciapmjdicmlobgbbm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
