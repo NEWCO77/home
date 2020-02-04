fbdff6c04c580e87520b71695471b1d8187292e7
pjmurphy777 <pjmurphy77@gmail.com>
2020-02-03 20:20:50 -0500

    Update index.php

--- fossil/index.php
+++ fossil/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Fossil';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bmgphbagkemldhfhhgmppjfkeklclcbo';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
