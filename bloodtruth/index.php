91895002f456bac4f1940608fa846a075c8ffdbe
pjmurphy777 <pjmurphy77@gmail.com>
2020-02-04 10:05:03 -0500

    Update index.php

--- bloodtruth/index.php
+++ bloodtruth/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Blood Truth Game';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'cemjpbidlmokjcdjniakkadddooblcoa';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
