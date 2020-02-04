57b375dbefbcfca7fff368937f047ef37657b411
pjmurphy777 <pjmurphy77@gmail.com>
2020-02-03 20:28:59 -0500

    Update index.php

--- davines/index.php
+++ davines/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Davines';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'laikkajhfhnnbigomhdfhcnioabkomnm';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
