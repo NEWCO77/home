9a876539a6b8b718021f89d5bc6a577a6ac8d835
pjmurphy777 <pjmurphy77@gmail.com>
2020-02-04 09:03:53 -0500

    Update index.php

--- ryangarcia/index.php
+++ ryangarcia/index.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Ryan Garcia';
 // After extension is loaded to the Chrome store, place the extension ID here.
-$extensionID = '';
+$extensionID = 'bjanmlefbgmndlijmokmbeecmghednif';
 $searchExtensionID = '';
 $tid = basename(__DIR__);
 include('../homepage.php');
